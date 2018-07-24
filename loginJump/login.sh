75d4d03a30c43f579dc719994f33441f1f9a47ff
mazhou <mazhoumz@gmail.com>
2018-07-24 15:13:17 +0800

    del some file

--- loginJump/login.sh
+++ loginJump/login.sh
@@ -1,26 +1,69 @@
 #!/bin/bash
 
-jump_user=$1
-jump_pwd=$2
-target_server=$3
-target_pwd=$4
+ip_csv="/Users/mazhou/workspace/git/shell/loginJump/asset/ipxshellIP.csv"
 
-/usr/bin/expect -c "
-    spawn ssh -o StrictHostKeyChecking=no $jump_user@jumpserverip -p jumpserverport;
-    set timeout 10;
-    expect \"*assword*\";
-    send \"$jump_pwd\r\";
-    expect \"USMshell\";
-    send \"ssh root@$target_server\r\";
-    set timeout 10;
-    expect \"*assword*\";
-    send \"$target_pwd\r\";
+if [ $# == 0 ];then
+    echo "no param,you can type show"
+    exit 0
+fi
 
-    expect {
-      \"*yes/no*\" {set timeout 10;send \"yes\r\";}
-      \"#\" {}
-    }
+[ ! -f $ip_csv ] && { echo "$INPUT file not found"; exit 0; }
+
+if [ "$1" == "show" ];then
+    if [ $# == 2 ];then
+        ip2="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$2")"
+        if [ "$ip2" == "" ];then
+            while IFS=, read desc user ip port pwd inIp inPort 
+            do
+                if test "${ip#*$2}" != "$ip"
+                then
+                    echo "$desc $ip:$port"
+                fi
+                if test "${desc#*$2}" != "$desc"
+                then
+                    echo "$desc $ip:$port"
+                fi
+            done < $ip_csv
+        else
+            while IFS=, read desc user ip port pwd inIp inPort 
+            do
+                if test "${ip#*$2}" != "$ip"
+                then
+                    echo "$desc $ip:$port $pwd $inIp $inPort"
+                    break    # $substring is in $string
+                fi
+            done < $ip_csv
+        fi
+    else
+        while IFS=, read desc user ip port pwd inIp inPort 
+        do
+            # 判断变量是否为空
+            if [ -z "$port" ];then
+                echo "$desc $ip"
+            else
+                echo "$desc $ip:$port"
+            fi
+            
+        done < $ip_csv
+    fi
+    exit 0
+fi
+
+ip="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$1")"
+
+if [ "$ip" == "" ];then
+    echo "no record or ip err";
+    exit 0
+else    
+    while IFS=, read desc user ip port pwd inIp inPort
+    do
+        if test "${ip#*$1}" != "$ip"
+        then
+            break    # $substring is in $string
+        fi
+    done < $ip_csv
+fi
 
 
-    interact;
-    "
+loginAct="$(cd "$(dirname "$0")" && pwd)/loginAct.sh"
+$loginAct $user $ip "$port" "$pwd"
